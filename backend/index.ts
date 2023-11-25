a8f8d7368580eca8c3c8be802c6eb5f51308e9e9
arapsingh <buip930@gmail.com>
2023-11-26 01:07:34 +0700

    no change

--- backend/index.ts
+++ backend/index.ts
@@ -8,12 +8,12 @@ const app: Application = express();
 const port: number = configs.general.PORT;
 
 const corsOptions = {
-    "Access-Control-Allow-Origin": "*",
+    "access-control-allow-origin": "https://utemy-cloud-frontend.vercel.app",
     origin: "*",
-    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+    optionsSuccessStatus: 200,
 };
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
