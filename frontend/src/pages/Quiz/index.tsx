02865da40b408257663e68ba0388f507a9d25dfb
NhanNguyen11002 <ntnhan11002@gmail.com>
2023-12-06 19:57:05 +0700

    create file

---
+++ frontend/src/pages/Quiz/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+// import { orderLesson } from "../../types/lesson";
+const Quiz: React.FC = () => {
+    return (
+        <>
+            {/* {isLoading && <Spin />} */}
+
+            <div className=" w-full  mt-[66px] mb-[100px] justify-center  flex flex-col"></div>
+        </>
+    );
+};
+
+export default Quiz;
