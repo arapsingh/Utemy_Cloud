7003bde319b0d6ca6fd21b386ae9d20960bbf567
arapsingh <buip930@gmail.com>
2023-12-08 02:58:42 +0700

    url dự phòng

--- frontend/src/api-config/apiCaller.ts
+++ frontend/src/api-config/apiCaller.ts
@@ -7,35 +7,64 @@ const axiosPublic = axios.create({
     baseURL: process.env.API_APP_BASE_URL || "https://utemyvietnam.cfapps.ap21.hana.ondemand.com",
 });
 
-const axiosInstance = axios.create();
+const axiosSecondary = axios.create({
+    baseURL: "https://utemy.cfapps.ap21.hana.ondemand.com",
+});
+
 axiosPublic.interceptors.response.use(
     (response) => response,
-    async (error: any) => {
+    async (error) => {
         const config = error?.config;
+
+        // Xử lý khi lỗi là 401 và chưa thử lại
         if (error?.response?.status === 401 && !config._retry) {
             config._retry = true;
-            const response = await apis.authApis.refreshAccessToken();
-            const accessToken = response.data.data.accessToken;
-            if (accessToken) {
-                Cookies.set("accessToken", accessToken);
-                config.headers = {
-                    ...config.headers,
-                    authorization: `Bearer ${accessToken}`,
-                };
-                return axiosInstance(config);
+            try {
+                const response = await apis.authApis.refreshAccessToken();
+                const accessToken = response.data.data.accessToken;
+                if (accessToken) {
+                    Cookies.set("accessToken", accessToken);
+                    config.headers = {
+                        ...config.headers,
+                        authorization: `Bearer ${accessToken}`,
+                    };
+                    // Thử lại từ axiosPublic
+                    return axiosPublic(config);
+                }
+            } catch (refreshError) {
+                // Nếu thử lại từ axiosPublic không thành công, thử lại từ axiosSecondary
+                try {
+                    const response = await axiosSecondary(config);
+                    return response;
+                } catch (secondaryError) {
+                    return Promise.reject(secondaryError);
+                }
             }
         }
-        if (error) {
-            if (error.response.data.message === constants.error.ERROR_LOGIN_AGAIN) {
-                Cookies.remove("refreshToken");
-                Cookies.remove("accessToken");
-                window.location.href = "/";
+
+        // Xử lý khi lỗi là 404
+        if (error?.response?.status === 404) {
+            // Thử lại từ axiosSecondary
+            try {
+                const response = await axiosSecondary(config);
+                return response;
+            } catch (secondaryError) {
+                return Promise.reject(secondaryError);
             }
-            return Promise.reject(error.response);
         }
-        // return error;
-    },
+
+        // Xử lý khi có lỗi khác
+        if (error.response && error.response.data.message === constants.error.ERROR_LOGIN_AGAIN) {
+            Cookies.remove("refreshToken");
+            Cookies.remove("accessToken");
+            window.location.href = "/";
+        }
+
+        // Trả về lỗi
+        return Promise.reject(error.response);
+    }
 );
+
 axiosPublic.interceptors.request.use(
     async (config: any) => {
         const accessToken = Cookies.get("accessToken");
@@ -48,35 +77,77 @@ axiosPublic.interceptors.request.use(
 
         return config;
     },
-    (error) => Promise.reject(error),
+    async (error) => {
+        // Xử lý lỗi trong interceptor request
+        if (error.response && error.response.status === 404) {
+            // Kiểm tra axiosPublic có tồn tại không
+            if (!axiosPublic) {
+                // Nếu không, thực hiện request từ axiosSecondary
+                try {
+                    const response = await axiosSecondary.request(error.config);
+                    return response;
+                } catch (secondaryError) {
+                    // Xử lý lỗi từ axiosSecondary (nếu cần)
+                    return Promise.reject(secondaryError);
+                }
+            }
+        }
+
+        // Trả về lỗi trong trường hợp khác
+        return Promise.reject(error);
+    }
 );
 
-export const apiCaller = (method: string, path: string, data?: any) => {
+
+export const apiCaller = async (method: string, path: string, data?: any) => {
     const refreshToken = Cookies.get("refreshToken");
-    return axiosPublic({
-        method,
-        headers: {
-            "Access-Control-Allow-Credentials": true,
-            "Access-Control-Allow-Origin": "*",
-            rftoken: `rfToken ${refreshToken}`,
-        },
-        url: `/api/${path}`,
-        data,
-    });
+    
+    // Kiểm tra xem axiosPublic có tồn tại không
+    const axiosInstance = axiosPublic || axiosSecondary;
+
+    try {
+        const response = await axiosInstance({
+            method,
+            headers: {
+                "Access-Control-Allow-Credentials": true,
+                "Access-Control-Allow-Origin": "*",
+                rftoken: `rfToken ${refreshToken}`,
+            },
+            url: `/api/${path}`,
+            data,
+        });
+
+        return response;
+    } catch (error) {
+        // Xử lý lỗi (nếu cần)
+        return Promise.reject(error);
+    }
 };
-export const apiCallerVnpay = (method: string, path: string, data?: any) => {
+
+export const apiCallerVnpay = async (method: string, path: string, data?: any) => {
     const refreshToken = Cookies.get("refreshToken");
-    return axiosPublic({
-        method,
-        headers: {
-            "Access-Control-Allow-Credentials": true,
-            "Access-Control-Allow-Origin": "*",
-            // "Origin:": "https://sandbox.vnpayment.vn",
-            rftoken: `rfToken ${refreshToken}`,
-        },
-        url: `/${path}`,
-        data,
-    });
+    
+    // Kiểm tra xem axiosPublic có tồn tại không
+    const axiosInstance = axiosPublic || axiosSecondary;
+
+    try {
+        const response = await axiosInstance({
+            method,
+            headers: {
+                "Access-Control-Allow-Credentials": true,
+                "Access-Control-Allow-Origin": "*",
+                rftoken: `rfToken ${refreshToken}`,
+            },
+            url: `/${path}`,
+            data,
+        });
+
+        return response;
+    } catch (error) {
+        // Xử lý lỗi (nếu cần)
+        return Promise.reject(error);
+    }
 };
+
 
 export default apiCaller;
