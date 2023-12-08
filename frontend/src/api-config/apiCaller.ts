05b0ae27e39a4bcb3f1a1440e6f3c6fc8fa8db7b
arapsingh <buip930@gmail.com>
2023-12-09 03:42:05 +0700

    last fix1

--- frontend/src/api-config/apiCaller.ts
+++ frontend/src/api-config/apiCaller.ts
@@ -3,7 +3,7 @@ import constants from "../constants";
 import apis from "../api";
 import Cookies from "js-cookie";
 const axiosPublic = axios.create({
-    baseURL: process.env.API_APP_BASE_URL || "https://utemy.cfapps.ap21.hana.ondemand.com",
+    baseURL: process.env.API_APP_BASE_URL || "https://utemyvietnam.cfapps.ap21.hana.ondemand.com",
 });
 
 const axiosInstance = axios.create();
