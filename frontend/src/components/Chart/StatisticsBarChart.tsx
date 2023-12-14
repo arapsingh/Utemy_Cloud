630997d237cc6285e8ab621178d6bf9c116449f0
arapsingh <buip930@gmail.com>
2023-12-15 03:59:16 +0700

    fix10

--- frontend/src/components/Chart/StatisticsBarChart.tsx
+++ frontend/src/components/Chart/StatisticsBarChart.tsx
@@ -47,19 +47,19 @@ const StatisticsBarChart: React.FC<StatisticBarChartProps> = ({
         },
     };
     return (
-        <Card className="border border-blue-gray-100 shadow-sm">
-            <CardHeader variant="gradient" color={color as color} floated={false} shadow={false}>
+        <Card className="border border-blue-gray-100 shadow-sm" placeholder={undefined}>
+            <CardHeader variant="gradient" color={color as color} floated={false} shadow={false} placeholder={undefined}>
                 <Chart {...chart} />
             </CardHeader>
-            <CardBody className="px-6 pt-0">
-                <Typography variant="h6" color="blue-gray">
+            <CardBody className="px-6 pt-0" placeholder={undefined}>
+                <Typography variant="h6" color="blue-gray" placeholder={undefined}>
                     {title}
                 </Typography>
-                <Typography variant="small" className="font-normal text-blue-gray-600">
+                <Typography variant="small" className="font-normal text-blue-gray-600" placeholder={undefined}>
                     {description}
                 </Typography>
             </CardBody>
-            {footer && <CardFooter className="border-t border-blue-gray-50 px-6 py-5">{footer}</CardFooter>}
+            {footer && <CardFooter className="border-t border-blue-gray-50 px-6 py-5" placeholder={undefined}>{footer}</CardFooter>}
         </Card>
     );
 };
