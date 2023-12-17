ac7fb3f9d93b16b358687991f0a598ea862bb95d
NhanNguyen11002 <ntnhan11002@gmail.com>
2023-12-17 14:59:34 +0700

    add negative number validation

--- frontend/src/validations/lesson.ts
+++ frontend/src/validations/lesson.ts
@@ -3,14 +3,14 @@ import constants from "../constants";
 
 export const addLessonValidationSchema = Yup.object({
     title: Yup.string().trim().required(constants.error.ERROR_TITLE_REQUIRED).max(100, constants.error.ERROR_TITLE_MAX),
-    duration: Yup.number().required(constants.error.ERROR_DURATION_REQUIRED),
+    duration: Yup.number().positive().required(constants.error.ERROR_DURATION_REQUIRED),
     description: Yup.string().trim().required(constants.error.ERROR_DESCRIPTION_REQUIRED),
 });
 export const addTestValidationSchema = Yup.object({
     title: Yup.string().trim().required(constants.error.ERROR_TITLE_REQUIRED).max(100, constants.error.ERROR_TITLE_MAX),
-    duration: Yup.number().required(constants.error.ERROR_DURATION_REQUIRED),
+    duration: Yup.number().positive().required(constants.error.ERROR_DURATION_REQUIRED),
     description: Yup.string().trim().required(constants.error.ERROR_DESCRIPTION_REQUIRED),
-    pass_percent: Yup.number().required(constants.error.ERROR_PASS_PERCENT_REQUIRED),
+    pass_percent: Yup.number().positive().required(constants.error.ERROR_PASS_PERCENT_REQUIRED),
     is_time_limit: Yup.boolean().required(constants.error.ERROR_IS_TIME_LIMIT_REQUIRED),
     quiz_group_id: Yup.number().required(constants.error.ERROR_QUIZ_GROUP_ID_REQUIRED),
 });
