b1ddfb269004a00323e66b99e5ac3d3cd8daa87a
arapsingh <buip930@gmail.com>
2024-06-04 21:16:51 +0700

    fixx

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Provider } from "react-redux";
@@ -9,7 +10,9 @@ window.katex = katex;
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
+    <React.StrictMode>
     <Provider store={store}>
         <App />
     </Provider>,
+    </React.StrictMode>
 );
