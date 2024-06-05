3509f0bf8af527e86b33c9a4ebb96e97807d8aca
arapsingh <buip930@gmail.com>
2024-06-06 01:41:28 +0700

    videoazure1

--- frontend/src/components/VideoPlayerForTrailerTrial.tsx
+++ frontend/src/components/VideoPlayerForTrailerTrial.tsx
@@ -25,65 +25,63 @@ export const VideoJS: React.FC<VideoJSType> = (props) => {
 
     useEffect(() => {
         const videoElement = videoRef.current;
-
+        console.log("url storage blog:", AZURE_BLOB_STORAGE_URL);
         if (videoElement) {
             if (Hls.isSupported()) {
                 const hls = new Hls();
-                if (isAzureBlobStorageUrl(props.source)) {
-                    // Nếu là URL từ Azure Blob Storage
-                    hls.loadSource(props.source);
-                } else {
-                    // Nếu không phải là URL từ Azure Blob Storage
-                    hls.attachMedia(videoElement);
-                }
-
+                hls.loadSource(props.source);
                 hls.on(Hls.Events.MANIFEST_PARSED, (event, data) => {
                     window.hls = hls;
-                    if (isAzureBlobStorageUrl(props.source)) {
-                        const availableQualities = hls.levels.map((l) => l.height);
-                        const defaultOptions: Plyr.Options = {
-                            controls: [
-                                "restart", // Restart playback
-                                "rewind", // Rewind by the seek time (default 10 seconds)
-                                "play", // Play/pause playback
-                                "fast-forward", // Fast forward by the seek time (default 10 seconds)
-                                "progress", // The progress bar and scrubber for playback and buffering
-                                "current-time", // The current time of playback
-                                "duration", // The full duration of the media
-                                "mute", // Toggle mute
-                                "volume", // Volume control
-                                "captions", // Toggle captions
-                                "settings", // Settings menu
-                                "pip", // Picture-in-picture (currently Safari only)
-                                "airplay", // Airplay (currently Safari only)
-                                "fullscreen", // Toggle fullscreen
-                            ],
+                    const availableQualities = hls.levels.map((l) => l.height);
+                    const defaultOptions: Plyr.Options = {
+                        controls: [
+                            "restart", // Restart playback
+                            "rewind", // Rewind by the seek time (default 10 seconds)
+                            "play", // Play/pause playback
+                            "fast-forward", // Fast forward by the seek time (default 10 seconds)
+                            "progress", // The progress bar and scrubber for playback and buffering
+                            "current-time", // The current time of playback
+                            "duration", // The full duration of the media
+                            "mute", // Toggle mute
+                            "volume", // Volume control
+                            "captions", // Toggle captions
+                            "settings", // Settings menu
+                            "pip", // Picture-in-picture (currently Safari only)
+                            "airplay", // Airplay (currently Safari only)
+                            "fullscreen", // Toggle fullscreen
+                        ],
 
-                            quality: {
-                                default: availableQualities[availableQualities.length - 1],
-                                options: availableQualities,
-                                forced: true,
-                                onChange: (event) => updateQuality(event),
-                            },
-                        };
-                        new Plyr(videoElement, defaultOptions);
-                    }
+                        quality: {
+                            default: availableQualities[availableQualities.length - 1],
+                            options: availableQualities,
+                            forced: true,
+                            onChange: (event) => updateQuality(event),
+                        },
+                    };
+                    new Plyr(videoElement, defaultOptions);
                 });
+
+                hls.attachMedia(videoElement);
             }
         }
     }, [props.source]);
-
-    // Hàm kiểm tra URL có phải từ Azure Blob Storage hay không
     const isAzureBlobStorageUrl = (url: string): boolean => {
-        // Thực hiện kiểm tra logic tại đây
-        // Đây là một ví dụ cơ bản, bạn có thể mở rộng và tùy chỉnh theo nhu cầu của mình
-        return url.startsWith(AZURE_BLOB_STORAGE_URL);;
+        return url.startsWith(AZURE_BLOB_STORAGE_URL);
     };
-    return (
-        <div className="w-full flex-1 shrink-0 text-white">
-            <video className="w-full h-[480px]" ref={videoRef} controls={true}></video>
-        </div>
-    );
+    const renderVideoElement = () => {
+        if (isAzureBlobStorageUrl(props.source)) {
+            return (
+                <video className="w-full h-[480px]" controls>
+                    <source src={props.source} type="video/mp4" />
+                    Your browser does not support the video tag.
+                </video>
+            );
+        } else {
+            return <video className="w-full h-[480px]" ref={videoRef} controls></video>;
+        }
+    };
+
+    return <div className="w-full flex-1 shrink-0 text-white">{renderVideoElement()}</div>;
 };
 
 export default VideoJS;
