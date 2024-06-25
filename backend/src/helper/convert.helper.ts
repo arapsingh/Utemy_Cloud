b75e453ad63b155b4f7f6ff42104c38535790bfe
arapsingh <buip930@gmail.com>
2024-06-25 19:51:43 +0700

    fix edit file in course

--- backend/src/helper/convert.helper.ts
+++ backend/src/helper/convert.helper.ts
@@ -8,21 +8,21 @@ const convertFilePath = (filePath: string): string => {
         .replace(/\\/g, "//");
     return fullPath;
 };
-const deConvertFilePath = (filePath: string): string => {
-    const publicPath = filePath.split("3001")[1];
-    const fullPath = path.join(process.cwd(), `//public//${publicPath}`);
-    return fullPath;
-};
 // const deConvertFilePath = (filePath: string): string => {
-//     try {
-//         const url = new URL(filePath);
-//         const publicPath = url.pathname; // Lấy đường dẫn tương đối từ URL
-//         const fullPath = path.join(process.cwd(), `public${publicPath}`);
-//         return fullPath;
-//     } catch (error) {
-//         console.error("Invalid URL:", error);
-//         return '';
-//     }
+//     const publicPath = filePath.split("3001")[1];
+//     const fullPath = path.join(process.cwd(), `//public//${publicPath}`);
+//     return fullPath;
 // };
+const deConvertFilePath = (filePath: string): string => {
+    try {
+        const url = new URL(filePath);
+        const publicPath = url.pathname; // Lấy đường dẫn tương đối từ URL
+        const fullPath = path.join(process.cwd(), `public${publicPath}`);
+        return fullPath;
+    } catch (error) {
+        console.error("Invalid URL:", error);
+        return '';
+    }
+};
 const convert = { convertFilePath, deConvertFilePath };
 export default convert;
